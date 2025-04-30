97b3d97441569218250ba68afb5a23223a315438
widinsoncajas <adriancajas2002@gmail.com>
2025-04-29 21:32:35 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,29 +2,29 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Servir archivos estáticos
+// Configurar Express para servir archivos estáticos desde la carpeta "public/imagenes"
 app.use('/imagenes', express.static(path.join(__dirname, 'public', 'imagenes')));
 
-// Redirigir rutas a PHP
+// Redirigir rutas a archivos PHP
 app.get('/', (req, res) => {
-  res.redirect('http://localhost:8082/inicio.php');
+  res.redirect('http://localhost:8080/inicio.php');  // Redirige a la página PHP en tu servidor Apache
 });
 
 app.get('/GAMA_FAMILIAR/GAMA_FAMI.php', (req, res) => {
-  res.redirect('http://localhost:8082/GAMA_FAMI.php');
+  res.redirect('http://localhost:8080/GAMA_FAMI.php');
 });
 
 app.get('/GAMA_MEDIA/GAMA_MEDIAA.php', (req, res) => {
-  res.redirect('http://localhost:8082/GAMA_MEDIAA.php');
+  res.redirect('http://localhost:8080/GAMA_MEDIAA.php');
 });
 
-// Middleware para manejar errores 404
+// Middleware para manejar errores 404 (Página no encontrada)
 app.use((req, res) => {
   res.status(404).send('Página no encontrada');
 });
 
-// Establecer el puerto de escucha
+// Establecer el puerto en el que el servidor Express escuchará
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Servidor corriendo en puerto ${port}`);
+  console.log(`Servidor Express corriendo en puerto ${port}`);
 });
